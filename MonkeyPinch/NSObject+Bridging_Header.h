b5c04553be6558e5ea412bdac4999f759370fc02
javen123 <thisjave@gmail.com>
2014-10-10 12:59:15 -0500

    first commit

---
+++ MonkeyPinch/NSObject+Bridging_Header.h
@@ -0,0 +1,14 @@
+//
+//  NSObject+Bridging_Header.h
+//  MonkeyPinch
+//
+//  Created by user on 10/10/14.
+//  Copyright (c) 2014 Neva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIGestureRecognizerSubclass.h>
+
+@interface NSObject (Bridging_Header)
+
+@end
